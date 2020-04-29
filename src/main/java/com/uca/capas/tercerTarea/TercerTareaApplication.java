41c62be5b8af18184aad41b6d67146deb581a9ac
Antonio <00267016@uca.edu.sv>
2020-04-28 21:39:28 -0600

    first commit

---
+++ src/main/java/com/uca/capas/tercerTarea/TercerTareaApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas.tercerTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TercerTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TercerTareaApplication.class, args);
+	}
+
+}
